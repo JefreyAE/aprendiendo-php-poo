385721ca575fb714741d405167c521d37430600b
JefreyAE <JefreyAE@DESKTOP-E4FP447>
2019-12-17 22:05:06 -0600

    inicial

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,27 +1,42 @@
 <?php
-require_once 'controllers_autoload.php';
 
-if(isset($_GET['controller'])){
-    $nombre_controlador = $_GET['controller']."Controller";
-}else{
-    echo "La página que buscas no existe";
+require_once 'autoload.php';
+require_once 'config/parameters.php';
+require_once 'views/layout/header.php';
+require_once 'views/layout/sidebar.php';
+
+function show_error() {
+    $error = new ErrorController;
+    $error->index();
+}
+
+if (isset($_GET['controller'])) {
+    $nombre_controlador = $_GET['controller'] . "Controller";
+} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $nombre_controlador = controller_default;
+    $controlador = new $nombre_controlador;
+    $action_default = action_default;
+    $controlador->$action_default();
+} else {
+    show_error();
     exit();
 }
 
-if(class_exists($_GET['controller'].'Controller')){
-    $nombre_controlador = $_GET['controller']."Controller";
+
+if (@class_exists($_GET['controller'] . 'Controller')) {
     $controlador = new $nombre_controlador;
-    
-    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
-        $controlador->$action();    
-    }else{
-        echo "La página que buscas no existe Action.";
-    }  
-}else{
-    echo "La página que buscas no existe Controller.";
+        $controlador->$action();
+    }elseif (!isset($_GET['action'])) {     
+        $action_default = action_default;
+        $controlador->$action_default();
+    } else {
+        show_error();
+    }
 }
 
+require_once 'views/layout/footer.php';
 /*
 if(isset($_GET['controller']) && class_exists($_GET['controller'].'Controller')){
     $nombre_controlador = $_GET['controller']."Controller";
